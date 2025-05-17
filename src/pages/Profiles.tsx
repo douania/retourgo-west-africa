bbbcc5c25c77ffa543ab901452777161ea6ced9b
douania <sodatrasn@gmail.com>
2025-05-17 15:31:29 +0000

    Create Profiles.tsx

---
+++ src/pages/Profiles.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <h1>Profil de l'utilisateur</h1>
+      <p>Informations sur l'utilisateur à afficher ici.</p>
+    </div>
+  );
+};
+
+export default Profile;
